21846d6fc56c5109bf8defc2d967462f566d10b6
Vlad Zagorulko <vlad.zagorulko@protonmail.com>
2017-06-15 15:08:34 +0300

    Set JWT_ACCESS_TOKEN_EXPIRES

--- server/default_config.py
+++ server/default_config.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
 SECRET_KEY = 'do_not_use_this_key_in_production'
 SQLALCHEMY_DATABASE_URI = 'postgres://scrum:scrum@127.0.0.1/scrum'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
