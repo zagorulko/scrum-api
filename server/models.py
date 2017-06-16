d3d41a25236d14b807a539cdf2da73365969ee01
Vlad Zagorulko <vlad.zagorulko@protonmail.com>
2017-06-16 03:48:06 +0300

    Implement project endpoints

--- server/models.py
+++ server/models.py
@@ -60,8 +60,8 @@ class Project(db.Model):
 
     members = db.relationship('User', secondary=project_members,
                               back_populates='projects')
-    tasks = db.relationship('Task', back_populates='project')
     sprints = db.relationship('Sprint', back_populates='project')
+    tasks = db.relationship('Task', back_populates='project')
 
     alias = db.Column(db.String, index=True, unique=True, nullable=False)
     name = db.Column(db.String, nullable=False)
@@ -71,8 +71,28 @@ class Project(db.Model):
     ci_link = db.Column(db.String)
 
     def authorize(self, user_id):
-        if not self.members.contains(user_id):
+        if not db.session.query(project_members)\
+                         .filter_by(project_id=self.id, user_id=user_id)\
+                         .first():
             raise AccessDenied()
+
+class Sprint(db.Model):
+    __tablename__ = 'sprints'
+    id = db.Column(db.Integer, primary_key=True)
+
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
+    project = db.relationship('Project', back_populates='sprints')
+    tasks = db.relationship('Task', back_populates='sprint')
+
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    goal = db.Column(db.Text)
+
+    def is_completed(self):
+        pass
+
+    def authorize(self, user_id):
+        self.project.authorize(user_id)
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -87,42 +107,21 @@ class Task(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author = db.relationship('User')
     comments = db.relationship('Comment', back_populates='task')
-    responsible = db.relationship('User', secondary=task_assigments)
+    assignees = db.relationship('User', secondary=task_assigments)
 
-    order = db.Column(db.Integer, nullable=False)
     title = db.Column(db.String, nullable=False)
-    priority = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.Enum(TaskStatus), nullable=False)
-
-    # track = db.Column(db.String)
-    vcs_commit = db.Column(db.String)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.Enum(TaskStatus), default=TaskStatus.BACKLOG)
+    kind = db.Column(db.Enum(TaskKind), nullable=False)
+    priority = db.Column(db.Integer, default=0)
+    acceptance_criteria = db.Column(db.Text)
+    user_story = db.Column(db.Text)
     initial_estimate = db.Column(db.Integer)
+    vcs_commit = db.Column(db.String)
+    bt_ticket = db.Column(db.String)
     completion_date = db.Column(db.DateTime)
     time_spent = db.Column(db.Integer)
     effort = db.Column(db.Float)
-    kind = db.Column(db.Enum(TaskKind))
-    user_story = db.Column(db.Text)
-    acceptance_criteria = db.Column(db.Text)
-    bt_ticket = db.Column(db.String)
-
-    def authorize(self, user_id):
-        self.project.authorize(user_id)
-
-class Sprint(db.Model):
-    __tablename__ = 'sprints'
-    id = db.Column(db.Integer, primary_key=True)
-
-    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
-    project = db.relationship('Project', back_populates='sprints')
-    tasks = db.relationship('Task', back_populates='sprint')
-
-    order = db.Column(db.Integer, nullable=False)
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
-    goal = db.Column(db.Text)
-
-    def is_completed(self):
-        pass
 
     def authorize(self, user_id):
         self.project.authorize(user_id)
--- server/resources.py
+++ server/resources.py
@@ -1,57 +1,68 @@
-from flask import request
+from datetime import datetime
+
+from flask import jsonify, request
 from flask_jwt_extended import (JWTManager, jwt_required,
                                 create_access_token, get_jwt_identity)
-from flask_restful import Resource, Api
+from flask_restful import Api, Resource, reqparse
 
 from server import app, models
 
-api = Api(app, prefix='/v1', errors={
-    'AccessDenied': {
-        'message': 'Access denied',
-        'status': 403
-    },
-    'NotFound': {
-        'message': 'Not found',
-        'status': 404
-    }
-})
+api = Api(app, prefix='/v1')
 jwt = JWTManager(app)
 
 class NotFound(Exception):
     pass
+
+@app.errorhandler(NotFound)
+def handle_not_found(error):
+    response = jsonify({'message': 'Not found'})
+    response.status_code = 404
+    return response
+
+@app.errorhandler(models.AccessDenied)
+def handle_access_denied(error):
+    response = jsonify({'message': 'Access denied'})
+    response.status_code = 403
+    return response
 
 def load_project(project_alias):
     project = models.Project.query.filter_by(alias=project_alias).first()
     if not project:
         raise NotFound()
     project.authorize(get_jwt_identity())
+    return project
 
 def load_task(task_id):
     task = models.Task.query.get(task_id)
     if not task:
         raise NotFound()
     task.authorize(get_jwt_identity())
+    return task
 
 def load_sprint(sprint_id):
     sprint = models.Sprint.query.get(sprint_id)
     if not sprint:
         raise NotFound()
     sprint.authorize(get_jwt_identity())
+    return sprint
 
 def load_comment(comment_id):
     comment = models.Comment.query.get(comment_id)
     if not comment:
         raise NotFound()
     comment.authorize(get_jwt_identity())
+    return comment
 
 class Login(Resource):
     def post(self):
-        username = request.json.get('username', None)
-        password = request.json.get('password', None)
-        user = models.User.query.filter_by(username=username).first()
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        args = parser.parse_args()
+        user = models.User.query.filter_by(username=args.username).first()
         if not user:
             return {'msg': 'No such user'}, 401
-        if not user.verify_password(password):
+        if not user.verify_password(args.password):
             return {'msg': 'Invalid password'}, 401
         access_token = create_access_token(identity=user.id)
         return {'access_token': access_token}
@@ -93,6 +104,27 @@ class Project(Resource):
     @jwt_required
     def get(self, project_alias):
         project = load_project(project_alias)
+        return {
+            'alias': project.alias,
+            'name': project.name,
+            'description': project.description,
+            'vcs_link': project.vcs_link,
+            'bt_link': project.bt_link,
+            'ci_link': project.ci_link
+        }
+
+class ProjectMembers(Resource):
+    @jwt_required
+    def get(self, project_alias):
+        project = load_project(project_alias)
+        members = []
+        for member in project.members:
+            members.append({
+                'username': member.username,
+                'full_name': member.full_name,
+                'email': member.email
+            })
+        return {'members': members}
 
 class ProjectSprints(Resource):
     @jwt_required
@@ -111,6 +143,37 @@ class ProjectTasks(Resource):
     @jwt_required
     def post(self, project_alias):
         project = load_project(project_alias)
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('sprint_id', type=int)
+        parser.add_argument('parent_task_id', type=int)
+        parser.add_argument('title', type=str, required=True)
+        parser.add_argument('kind', type=models.TaskKind, required=True)
+        parser.add_argument('priority', type=int, default=0)
+        parser.add_argument('acceptance_criteria', type=str)
+        parser.add_argument('user_story', type=str)
+        parser.add_argument('initial_estimate', type=int)
+        parser.add_argument('assignees', type=str, nargs='*', default=[])
+        args = parser.parse_args()
+
+        task = models.Task()
+        task.project_id = project.id
+        task.sprint_id = args.sprint_id
+        task.parent_task_id = args.parent_task_id
+        task.author_id = get_jwt_identity()
+        task.title = args.title
+        task.creation_date = datetime.utcnow()
+        task.kind = args.kind
+        task.priority = args.priority
+        task.acceptance_criteria = args.acceptance_criteria
+        task.user_story = args.user_story
+        task.initial_estimate = args.initial_estimate
+        for assignee_username in args.assignees:
+            assignee = User.query.filter_by(username=assignee_username).first()
+            task.assignees.append(assignee.id)
+
+        models.db.session.add(task)
+        models.db.session.commit()
 
 class Sprint(Resource):
     @jwt_required
@@ -169,6 +232,7 @@ api.add_resource(Login, '/login')
 api.add_resource(User, '/user')
 api.add_resource(UserProjects, '/user/projects')
 api.add_resource(Project, '/projects/<string:project_alias>')
+api.add_resource(ProjectMembers, '/projects/<string:project_alias>/members')
 api.add_resource(ProjectSprints, '/projects/<string:project_alias>/sprints')
 api.add_resource(ProjectTasks, '/projects/<string:project_alias>/tasks')
 api.add_resource(Sprint, '/sprints/<int:sprint_id>')
